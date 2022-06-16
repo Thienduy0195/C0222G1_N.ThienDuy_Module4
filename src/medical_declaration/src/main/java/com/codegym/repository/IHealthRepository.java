f5ac4029142076a57191136dc86d764f0436709f
Thienduy0195 <thienduy0195@gmail.com>
2022-06-16 20:08:32 +0700

    ss4-excercise

--- src/medical_declaration/src/main/java/com/codegym/repository/IHealthRepository.java
+++
@@ -1,8 +0,0 @@
-package com.codegym.repository;
-
-import com.codegym.model.Health;
-
-public interface IHealthRepository {
-    void save(Health health);
-    Health getHealth();
-}
--- src/medical_declaration/src/main/java/com/codegym/service/IHealthService.java
+++
@@ -1,8 +0,0 @@
-package com.codegym.service;
-
-import com.codegym.model.Health;
-
-public interface IHealthService {
-    void save(Health health);
-    Health getHealth();
-}
